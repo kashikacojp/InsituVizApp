7ecf032d6193d457703dfe3c53ce7f56de5476e3
kioku <oku@kashika.co.jp>
2024-02-25 14:20:41 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <InsituVizApp />
+</template>
+
+<script>
+import InsituVizApp from './components/InsituVizApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InsituVizApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InsituVizApp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Insitu-vizualization</h1>
+    <div>
+      <img src="../assets/output_000000_000004.jpg">
+    </div>
+    <div>
+      <p>Time</p>
+      <input type="range" max="100" step="1" v-model="tmslider">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InsituViz',
+  props: {
+  },
+  data() {
+    return {
+      "tmslider": 10
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
