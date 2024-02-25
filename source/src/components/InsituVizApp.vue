492ab89d4a69680a812ff67e4a49287a9d3a704e
kioku <oku@kashika.co.jp>
2024-02-25 19:19:27 +0900

    update img view

--- source/src/components/InsituVizApp.vue
+++ source/src/components/InsituVizApp.vue
@@ -2,11 +2,11 @@
   <div>
     <h1>Insitu-vizualization</h1>
     <div>
-      <img src="../assets/output_000000_000004.jpg">
+      <img :src="files[parseInt(tmslider * (files.length-1) / 100)]">
     </div>
     <div>
       <p>Time</p>
-      <input type="range" max="100" step="1" v-model="tmslider">
+      <input type="range" max="100" step="1" v-model="tmslider" class="slider">
     </div>
   </div>
 </template>
@@ -18,14 +18,51 @@ export default {
   },
   data() {
     return {
-      "tmslider": 10
+      "tmslider": 10,
+      "files": []
     }
+  },
+  mounted() {
+    console.log('A')
+    fetch('/data/data.csv')
+    .then(response => {
+      // レスポンスが成功したかどうかを確認
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // レスポンスからテキストデータを取得して返す
+      return response.text();
+    })
+    .then(CSVRaw => {
+      const endOfFirstLineIndex = CSVRaw.indexOf("\n");
+      const headerRawLine = CSVRaw.slice(0, endOfFirstLineIndex);
+      const headers = headerRawLine.split(",");
+      console.log(headers)
+
+      const rowsRaw = CSVRaw.slice(endOfFirstLineIndex + 1);
+      const rowsNoCells = rowsRaw.split("\n");
+      const rows = rowsNoCells.map(row => row.split(","));
+      console.log(rows)
+
+      for (var i in rows){
+        console.log(rows[i][2])
+        this.files.push('../../data/' + rows[i][2])
+      }
+    })
+    .catch(error => {
+      // エラーが発生した場合の処理
+      console.error('There was a problem with the fetch operation:', error);
+    });
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.slider {
+  width: 250px;
+}
 h3 {
   margin: 40px 0 0;
 }
